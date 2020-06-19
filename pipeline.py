ad48cb5b6bcd00d4841435facca965232f599d8f
Jakob Bach <jakob.bach@kit.edu>
2020-06-19 10:49:04 +0200

    Move most of experiment pipeline into main
    ... which prevents copying that stuff into each sub-process on Windows
    (does not happen on Linux). In particular, if we have multiple datasets in
    future, this is crucial.

--- pipeline.py
+++ pipeline.py
@@ -3,20 +3,9 @@
 Main script for our experiments with synthetic constraints.
 """
 
-
-from multiprocessing import Pool
-
-import pandas as pd
-from sklearn.datasets import load_boston
-from tqdm import tqdm
-
 import combi_expressions
 import combi_solving
 import generation
-
-
-def update_progress(x):
-    progress_bar.update(n=1)
 
 
 def evaluate(constraint_type, generator_func, generator_args, dataset, qualities):
@@ -30,36 +19,49 @@ def evaluate(constraint_type, generator_func, generator_args, dataset, qualities
     return result
 
 
-dataset = load_boston()
-features = dataset['feature_names']
-X = pd.DataFrame(dataset['data'], columns=features)
-y = pd.Series(dataset['target'])
-qualities = [round(abs(X[feature].corr(y)), 2) for feature in features]
-datasets = [{'dataset': 'boston', 'qualities': qualities}]
-
-common_generator_args = {'num_repetitions': 10, 'min_num_constraints': 1, 'max_num_constraints': 10}
-generators = [
-    {'constraint_type': 'group_AT_LEAST', 'generator_func': 'AtLeastGenerator',
-     'generator_args': {**common_generator_args, 'global_at_most': 10}},
-    {'constraint_type': 'group_AT_MOST', 'generator_func': 'AtMostGenerator',
-     'generator_args': common_generator_args},
-    {'constraint_type': 'global_AT_MOST', 'generator_func': 'GlobalAtMostGenerator',
-     'generator_args': common_generator_args},
-    {'constraint_type': 'single_IFF', 'generator_func': 'IffGenerator',
-     'generator_args': {**common_generator_args, 'global_at_most': 10}},
-    {'constraint_type': 'group_IFF', 'generator_func': 'IffGenerator',
-     'generator_args': {**common_generator_args, 'global_at_most': 10, 'max_num_variables': 5}},
-    {'constraint_type': 'single_NAND', 'generator_func': 'NandGenerator',
-     'generator_args': common_generator_args},
-    {'constraint_type': 'group_NAND', 'generator_func': 'NandGenerator',
-     'generator_args': {**common_generator_args, 'max_num_variables': 5}},
-     {'constraint_type': 'single_XOR', 'generator_func': 'XorGenerator',
-     'generator_args': common_generator_args}
-]
-
-
+# use of__ main__ around multiprocessing is required on Windows and recommended on Linux;
+# prevents infinite recursion of spawning sub-processes
+# furthermore, on Windows, whole file is imported, so everything outside main copied into sub-processes,
+# while Linux sub-processes have access to all resources of the parent without copying
 if __name__ == '__main__':
+    from multiprocessing import Pool
+
+    import pandas as pd
+    from sklearn.datasets import load_boston
+    from tqdm import tqdm
+
+    dataset = load_boston()
+    features = dataset['feature_names']
+    X = pd.DataFrame(dataset['data'], columns=features)
+    y = pd.Series(dataset['target'])
+    qualities = [round(abs(X[feature].corr(y)), 2) for feature in features]
+    datasets = [{'dataset': 'boston', 'qualities': qualities}]
+
+    common_generator_args = {'num_repetitions': 10, 'min_num_constraints': 1, 'max_num_constraints': 10}
+    generators = [
+        {'constraint_type': 'group_AT_LEAST', 'generator_func': 'AtLeastGenerator',
+         'generator_args': {**common_generator_args, 'global_at_most': 10}},
+        {'constraint_type': 'group_AT_MOST', 'generator_func': 'AtMostGenerator',
+         'generator_args': common_generator_args},
+        {'constraint_type': 'global_AT_MOST', 'generator_func': 'GlobalAtMostGenerator',
+         'generator_args': common_generator_args},
+        {'constraint_type': 'single_IFF', 'generator_func': 'IffGenerator',
+         'generator_args': {**common_generator_args, 'global_at_most': 10}},
+        {'constraint_type': 'group_IFF', 'generator_func': 'IffGenerator',
+         'generator_args': {**common_generator_args, 'global_at_most': 10, 'max_num_variables': 5}},
+        {'constraint_type': 'single_NAND', 'generator_func': 'NandGenerator',
+         'generator_args': common_generator_args},
+        {'constraint_type': 'group_NAND', 'generator_func': 'NandGenerator',
+         'generator_args': {**common_generator_args, 'max_num_variables': 5}},
+        {'constraint_type': 'single_XOR', 'generator_func': 'XorGenerator',
+         'generator_args': common_generator_args}
+    ]
+
+    def update_progress(x):
+        progress_bar.update(n=1)
+
     progress_bar = tqdm(total=len(generators))
+
     process_pool = Pool()
     results = [process_pool.apply_async(evaluate, kwds={**x, **y}, callback=update_progress)
                for x in generators for y in datasets]
