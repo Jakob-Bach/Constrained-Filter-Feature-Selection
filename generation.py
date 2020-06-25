432ffa65b7b28ee7efcb24a92f6ae38b71b064dc
Jakob Bach <jakob.bach@kit.edu>
2020-06-25 08:31:28 +0200

    Allow relative number of features
    As we will support datasets with different numbers of features in future,
    we extend the constraint generators to also accept fractions for the number
    of features. The current experiment pipeline remains unchanged for now and
    still returns the same results as before.

--- generation.py
+++ generation.py
@@ -17,8 +17,8 @@ class ConstraintGenerator:
         self.problem = problem
         self.min_num_constraints = kwargs.get('min_num_constraints', 1)
         self.max_num_constraints = kwargs.get('max_num_constraints', 1)
-        self.min_num_variables = kwargs.get('min_num_variables', 2)
-        self.max_num_variables = kwargs.get('max_num_variables', 2)
+        self.min_num_variables = self.make_card_absolute(kwargs.get('min_num_variables', 2))
+        self.max_num_variables = self.make_card_absolute(kwargs.get('max_num_variables', 2))
         self.num_repetitions = kwargs.get('num_repetitions', 1)
         self.seed = 25
 
@@ -42,13 +42,26 @@ class ConstraintGenerator:
             self.problem.clear_constraints()
         return pd.DataFrame(results)
 
+    def make_card_absolute(self, cardinality, pass_none=False):
+        max_cardinality = len(self.problem.get_variables())
+        if cardinality is None:
+            if pass_none:  # None might be used as default for different purposes
+                return None
+            cardinality = max_cardinality  # problem-specifc upper bound
+        elif 0 < cardinality < 1:
+            cardinality = round(cardinality * max_cardinality)  # turn absolute
+        cardinality = int(cardinality)
+        if (cardinality < 1) or (cardinality > max_cardinality):
+            raise ValueError(f'Cardinality of {cardinality} is outside range [1,{max_cardinality}].')
+        return cardinality
+
 
 class AtLeastGenerator(ConstraintGenerator):
 
     def __init__(self, problem, global_at_most, cardinality=None, **kwargs):
         super().__init__(problem, **kwargs)
-        self.cardinality = cardinality
-        self.global_at_most = global_at_most
+        self.cardinality = self.make_card_absolute(cardinality, pass_none=True)
+        self.global_at_most = self.make_card_absolute(global_at_most)
 
     # As at_most does not exclude the trivial solution (select everything), we
     # also add a global cardinality constraint
@@ -70,7 +83,7 @@ class AtMostGenerator(ConstraintGenerator):
 
     def __init__(self, problem, cardinality=None, **kwargs):
         super().__init__(problem, **kwargs)
-        self.cardinality = cardinality
+        self.cardinality = self.make_card_absolute(cardinality, pass_none=True)
 
     def generate(self, variables):
         if self.cardinality is None:
@@ -102,7 +115,7 @@ class IffGenerator(ConstraintGenerator):
 
     def __init__(self, problem, global_at_most, **kwargs):
         super().__init__(problem, **kwargs)
-        self.global_at_most = global_at_most
+        self.global_at_most = self.make_card_absolute(global_at_most)
 
     # As iff does not exclude the trivial solution (select everything), we
     # also add a global cardinality constraint
