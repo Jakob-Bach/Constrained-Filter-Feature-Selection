04291c27d6d11c5613005cf6c67051928f9b833c
Jakob Bach <jakob.bach@kit.edu>
2020-07-07 16:35:07 +0200

    Add OpenML dataset preparation
    Download OpenML datasets according to certain criteria and bring them to a
    form suitable for our experiment pipeline.

---
+++ prepare_openml_datasets.py
@@ -0,0 +1,68 @@
+"""OpenML dataset preparation
+
+Script which downloads datasets from OpenML and saves them in a format suitable
+for the experiment pipeline.
+
+Usage: python prepare_openml_datasets.py --help
+"""
+
+import argparse
+import pathlib
+from typing import Sequence
+
+import numpy as np
+import openml
+import tqdm
+
+from data_utility import save_dataset
+
+
+def prepare_openml_datasets(data_dir: pathlib.Path, data_ids=Sequence[int]) -> None:
+    if not data_dir.is_dir():
+        print('Directory does not exist. We create it.')
+        data_dir.mkdir(parents=True)
+    if len(list(data_dir.glob('*'))) > 0:
+        print('Data directory is not empty. Files might be overwritten, but not deleted.')
+    if len(data_ids) == 0:
+        print('Getting an overview of datasets ...')
+        dataset_overview = openml.datasets.list_datasets(status='active', output_format='dataframe')
+        dataset_overview = dataset_overview[
+            (dataset_overview['format'] != 'Sparse_ARFF') &  # would break when calling get_data()
+            (dataset_overview['NumberOfClasses'] == 0) &
+            (dataset_overview['NumberOfNumericFeatures'] >= 10) &
+            (dataset_overview['NumberOfNumericFeatures'] <= 20) &
+            (dataset_overview['NumberOfInstances'] >= 100) &
+            (dataset_overview['NumberOfInstances'] <= 10000) &
+            (dataset_overview['NumberOfMissingValues'] == 0)
+        ]
+        dataset_overview = dataset_overview.sort_values(by='version').groupby('name').last()  # latest version
+        dataset_overview.to_csv(data_dir / '_data_overview.csv', index=True)  # indexed by name
+        data_ids = list(dataset_overview['did'])
+    else:
+        print('Using pre-defined dataset ids.')
+    print('Downloading datasets ...')
+    for data_id in tqdm.tqdm(data_ids):
+        dataset = openml.datasets.get_dataset(dataset_id=data_id, download_data=True)
+        dataset_name = dataset.name
+        X, y, _, _ = dataset.get_data(target=dataset.default_target_attribute)
+        assert np.issubdtype(y.dtype, np.number)  # regression problem
+        # Feature types are a bit tricky; there is a "categorical_indicator" returned by get_data(),
+        # but that is not reliable; "data_type" from class "OpenMLFeature" is better
+        numeric_cols = [feature.name for feature in dataset.features.values() if feature.data_type == 'numeric']
+        assert len(numeric_cols) == dataset_overview.loc[dataset_name].NumberOfNumericFeatures
+        # "dataset.features" also includes target and might include non-existing columns
+        X = X[[col for col in numeric_cols if col in list(X)]]  # drop other column types
+        save_dataset(X, y, dataset_name=dataset_name, directory=data_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Retrieves several datasets from OpenML, prepares them for the ' +
+        'experiment pipeline and stores them in the specified directory.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--directory', type=pathlib.Path, default='.', dest='data_dir',
+                        help='Output directory for data.')
+    parser.add_argument('-i', '--ids', type=int, default=[], nargs='*', dest='data_ids',
+                        help='Data ids. If none provided, will search for suitable datasets automatically.')
+    prepare_openml_datasets(**vars(parser.parse_args()))
+    print('Datasets prepared and saved.')
