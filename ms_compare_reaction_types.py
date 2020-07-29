102706ec261382404f1385aad69978a456461e98
Jakob Bach <jakob.bach@kit.edu>
2020-07-29 09:22:57 +0200

    Minor refactorings in comparison of reaction types

--- ms_compare_reaction_types.py
+++ ms_compare_reaction_types.py
@@ -6,27 +6,27 @@ Compare which reaction types domminate over the course of time.
 
 import matplotlib.pyplot as plt
 
-from ms_datasets import prepare_delta_voxel_data, prepare_sampled_voxel_data
+import ms_datasets
 
 SAVE_DIR = '../paper-cffs-material/Reaction-Fractions/'  # use None if plots should not be saved
-PLOT_AGGREGATES = ['mean', 'median']  # names of aggregationn function for DataFrame.agg()
+SPATIAL_AGGREGATES = ['mean', 'median']  # aggregate over voxels at each time step
+REACTION_TYPES = ['coll', 'cs', 'glissile', 'hirth', 'inplane', 'lomer', 'multiple_coll']
 
-sampled_voxel_dataset = prepare_sampled_voxel_data(delta_steps=20, subset='no')
-delta_voxel_dataset = prepare_delta_voxel_data(subset='no')
-reaction_types = ['coll', 'cs', 'glissile', 'hirth', 'inplane', 'lomer', 'multiple_coll']
-prediction_problems = [
-    {'name': 'sampled_voxel_n', 'dataset': sampled_voxel_dataset, 'reactions': reaction_types},
-    {'name': 'delta_voxel_n', 'dataset': delta_voxel_dataset, 'reactions': reaction_types},
-    {'name': 'delta_voxel_rho', 'dataset': delta_voxel_dataset, 'reactions': ['rho_' + x for x in reaction_types]},
+sampled_voxel_dataset = ms_datasets.prepare_sampled_voxel_data(delta_steps=20, subset='no')
+delta_voxel_dataset = ms_datasets.prepare_delta_voxel_data(subset='no')
+scenarios = [
+    {'name': 'sampled_voxel_n', 'dataset': sampled_voxel_dataset, 'reactions': REACTION_TYPES},
+    {'name': 'delta_voxel_n', 'dataset': delta_voxel_dataset, 'reactions': REACTION_TYPES},
+    {'name': 'delta_voxel_rho', 'dataset': delta_voxel_dataset, 'reactions': ['rho_' + x for x in REACTION_TYPES]},
 ]
 
-for problem in prediction_problems:
-    reaction_cols = problem['reactions']
-    name = problem['name']
-    dataset = problem['dataset'][reaction_cols + ['pos_x', 'pos_y', 'pos_z', 'time']].copy()
+for scenario in scenarios:
+    reaction_cols = scenario['reactions']
+    name = scenario['name']
+    dataset = scenario['dataset'][reaction_cols + ['pos_x', 'pos_y', 'pos_z', 'time']].copy()
     dataset['reactions_total'] = dataset[reaction_cols].sum(axis='columns')
     dataset[reaction_cols] = dataset[reaction_cols].apply(lambda x: x / dataset['reactions_total'] * 100)
-    for agg_func in PLOT_AGGREGATES:
+    for agg_func in SPATIAL_AGGREGATES:
         dataset.groupby('time')[reaction_cols].agg(agg_func).plot(
             title=f'{agg_func} fraction of reaction types for scenario "{name}"').set(ylabel='%')
         if SAVE_DIR is not None:
