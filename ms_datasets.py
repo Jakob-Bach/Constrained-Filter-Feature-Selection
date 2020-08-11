34c502d640e306f1b7d93b80fc28bdd6b42bd8f6
Jakob Bach <jakob.bach@kit.edu>
2020-08-11 12:40:01 +0200

    Improve time-step-handling in MS pipeline
    - extract common logic from the two dataset preparation functions
    - allow arbitrary prediction horizons also for delta-voxel data
    - handle prediction horizons spanning over gaps in time steps properly
    (previously, they always were excluded, now they may be kept)

--- ms_datasets.py
+++ ms_datasets.py
@@ -5,61 +5,73 @@ Utility functions for loading datasets for our case study in materials science.
 
 
 import re
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Sequence
 
 import pandas as pd
 
 
+REACTION_TYPES = ['coll', 'cs', 'glissile', 'hirth', 'inplane', 'lomer', 'multiple_coll']
+
+
+def determine_valid_times(times: pd.Series, delta_steps: int = 0) -> pd.DataFrame:
+    result = pd.DataFrame({'time': times.sort_values().unique()})
+    # A time is valid if we also have time + prediction horizon available
+    result['target_time'] = result['time'] + delta_steps * 50
+    result['is_valid'] = result['target_time'].isin(result['time'])
+    # Determine groups of consecutive valid time steps, i.e., group begins with valid time steps
+    # where prior time step is invalid or more than 50 time units away
+    result['valid_group'] = (result['is_valid'] &
+                             (~result['is_valid'].shift().fillna(False) |
+                              (result['time'] - result['time'].shift() != 50))).cumsum()
+    result.loc[~result['is_valid'], 'valid_group'] = 0
+    return result
+
+
+# Dataset is modified in place
+def add_deltas_and_sanitize_time(dataset: pd.DataFrame, delta_quantities: Sequence[str] = REACTION_TYPES,
+                                 delta_steps: int = 1, subset: str = 'none'):
+    time_validity_info = determine_valid_times(times=dataset['time'], delta_steps=delta_steps)
+    # Add delta features (forward delta, difference between reaction density "delta_steps" in future
+    # and reaction density now); as there are missing time steps, we cannot use shift()
+    dataset.sort_values(by=['time', 'pos_x', 'pos_y', 'pos_z'], inplace=True)
+    for time, target_time in zip(time_validity_info.loc[time_validity_info['is_valid'], 'time'],
+                                 time_validity_info.loc[time_validity_info['is_valid'], 'target_time']):
+        for quantity in delta_quantities:
+            dataset.loc[dataset['time'] == time, 'delta_' + quantity] =\
+                dataset.loc[dataset['time'] == target_time, quantity].values -\
+                    dataset.loc[dataset['time'] == time, quantity].values
+    # Remove invalid time steps
+    if subset == 'consecutive':  # consider longest sequence without gaps
+        largest_group = time_validity_info[time_validity_info['is_valid']].groupby('valid_group').size().idxmax()
+        timestamps = time_validity_info.loc[time_validity_info['valid_group'] == largest_group, 'time']
+    elif subset == 'complete':  # consider all time steps where prediction can be made
+        timestamps = time_validity_info.loc[time_validity_info['is_valid'], 'time']
+    else:  # use all time steps
+        timestamps = time_validity_info['time']
+    dataset.drop(dataset[~dataset['time'].isin(timestamps)].index, inplace=True)
+
+
 def prepare_delta_voxel_data(
         path: str = 'C:/MyData/Versetzungsdaten/Voxel_Data/delta_sampled_merged_last_voxel_data_size2400_order2_speedUp2.csv',
-        subset: str = 'consecutive') -> pd.DataFrame:
+        delta_steps: int = 1, subset: str = 'none') -> pd.DataFrame:
     dataset = pd.read_csv(path)
     dataset.drop(columns=dataset.columns[0], inplace=True)  # drop 1st column (unnamed id column)
-    # Add summed reaction densities:
-    for quantity in ['coll', 'cs', 'glissile', 'hirth', 'inplane', 'lomer', 'multiple_coll']:
-        col = 'rho_' + quantity
-        dataset[col] = dataset[[col + '_' + str(i) for i in range(1, 13)]].sum(axis='columns')
-        col = 'delta_rho_' + quantity
-        dataset[col] = dataset[[col + '_' + str(i) for i in range(1, 13)]].sum(axis='columns')
-    # Remove invalid time steps (optional)
-    times = pd.DataFrame({'time': dataset['time'].sort_values().unique()})
-    times['time_diff'] = times - times.shift()
-    if subset == 'consecutive':  # consider longest sequence without gaps
-        times['group'] = (times['time_diff'] != 50).cumsum()  # macro-time step bigger than usual
-        longest_group = times.groupby('group').size().idxmax()
-        timestamps = list(times[times['group'] == longest_group].time)[1:]  # longest sequence, without first element
-    elif subset == 'complete':  # consider all time steps where prediction can be made
-        timestamps = list(times[times['time_diff'] == 50]['time'])
-    else:  # use all time steps
-        timestamps = times['time']
-    dataset = dataset[dataset.time.isin(timestamps)]
+    quantities = ['rho_' + x for x in REACTION_TYPES]
+    for quantity in quantities:  # overall density not available per default, so compute it
+        dataset[quantity] = dataset[[quantity + '_' + str(i) for i in range(1, 13)]].sum(axis='columns')
+    add_deltas_and_sanitize_time(dataset, delta_quantities=quantities,
+                                 delta_steps=delta_steps, subset=subset)
     return dataset
 
 
 def prepare_sampled_voxel_data(
         path: str = 'C:/MyData/Versetzungsdaten/Voxel_Data/sampled_voxel_data_size2400_order2_speedUp2.csv',
-        delta_steps: int = 1, subset: str = 'consecutive') -> pd.DataFrame:
+        delta_steps: int = 1, subset: str = 'none') -> pd.DataFrame:
     dataset = pd.read_csv(path)
     dataset.drop(columns=dataset.columns[0], inplace=True)  # drop 1st column (unnamed id column)
     dataset = dataset[dataset['time'] % 50 == 0]  # remove irregular time steps
-    # Add delta features
-    dataset.sort_values(by='time', inplace=True)
-    dataset_grouped = dataset.groupby(['pos_x', 'pos_y', 'pos_z'])
-    for quantity in ['coll', 'glissile', 'lomer']:
-        dataset['delta_' + quantity] = dataset_grouped[quantity].transform(lambda x: x.shift(-delta_steps) - x)
-    # Remove invalid time steps (optional)
-    times = pd.DataFrame({'time': dataset['time'].sort_values().unique()})
-    times['time_diff'] = times - times.shift()
-    times['group'] = (times['time_diff'] != 50).cumsum()  # macro-time step unusual
-    if subset == 'consecutive':  # consider longest sequence without gaps
-        longest_group = times.groupby('group').size().idxmax()
-        timestamps = list(times[times['group'] == longest_group].time)[:-delta_steps]
-    elif subset == 'complete':  # consider all time steps where prediction can be made
-        # For each group, remove the last "delta_steps" elements:
-        timestamps = list(times.groupby('group').apply(lambda x: x.drop(labels=x.index[-delta_steps:]))['time'])
-    else:  # use all time steps
-        timestamps = times['time']
-    dataset = dataset[dataset.time.isin(timestamps)]
+    add_deltas_and_sanitize_time(dataset, delta_quantities=REACTION_TYPES,
+                                 delta_steps=delta_steps, subset=subset)
     return dataset
 
 
@@ -79,7 +91,7 @@ def predict_delta_voxel_data_relative(dataset: pd.DataFrame, dataset_name: str =
                                       reaction_type: str = 'glissile') -> Dict[str, Any]:
     target = 'delta_rho_' + reaction_type
     features = [x for x in list(dataset) if target not in x]  # exclude if feature name contains the target string
-    features = [x for x in features if re.search('^0_', x) is not None]  # only values from previous time step
+    features = [x for x in features if re.search('^([0-9]+)_', x) is None]  # exclude historic feature
     return {'dataset_name': dataset_name, 'dataset': dataset[dataset['rho_' + reaction_type] != 0],
             'target': target, 'features': features}
 
