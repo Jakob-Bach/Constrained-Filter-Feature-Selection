e8eb8c00f1856a166981fa8f05ba68faa54c564d
Jakob Bach <jakob.bach@kit.edu>
2020-08-21 17:01:44 +0200

    Create plots to compare evaluation metric over datasets

--- pipeline_evaluation.py
+++ pipeline_evaluation.py
@@ -12,28 +12,28 @@ import seaborn as sns
 results = pd.read_csv('data/openml-results/results.csv')
 CONSTRAINT_METRICS = ['objective_value', 'num_selected', 'num_constraints', 'frac_solutions']
 PREDICTION_METRICS = [x for x in results.columns if x.endswith('_r2')]
+EVALUATION_METRICS = CONSTRAINT_METRICS + ['xgb-tree_test_r2']
 
 # ---Distribution of constraint evaluation metrics, comparing constraint types---
 
-for constraint_metric in CONSTRAINT_METRICS + ['xgb-tree_test_r2']:
-    sns.boxplot(x='constraint_name', y=constraint_metric, data=results)
+for evaluation_metric in EVALUATION_METRICS:
+    sns.boxplot(x='constraint_name', y=evaluation_metric, data=results)
     plt.xticks(rotation=45)
-    if constraint_metric.endswith('_r2'):
+    if evaluation_metric.endswith('_r2'):
         plt.ylim(-0.1, 1.1)
     plt.show()
 
 # ---Relationship between constraint evaluation metrics---
 
-evaluation_metrics = CONSTRAINT_METRICS + ['xgb-tree_test_r2']
-for i in range(len(evaluation_metrics) - 1):
-    for j in range(i + 1, len(evaluation_metrics)):
-        results.plot.scatter(x=evaluation_metrics[i], y=evaluation_metrics[j], s=1)
-        if evaluation_metrics[j].endswith('_r2'):
+for i in range(len(EVALUATION_METRICS) - 1):
+    for j in range(i + 1, len(EVALUATION_METRICS)):
+        results.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j], s=1)
+        if EVALUATION_METRICS[j].endswith('_r2'):
             plt.ylim(-0.1, 1.1)
         plt.show()
-# All in one: sns.pairplot(results[evaluation_metrics])
+# All in one: sns.pairplot(results[EVALUATION_METRICS])
 
-sns.heatmap(data=results[evaluation_metrics].corr(method='spearman'), vmin=-1, vmax=1,
+sns.heatmap(data=results[EVALUATION_METRICS].corr(method='spearman'), vmin=-1, vmax=1,
             cmap='RdYlGn', annot=True, square=True)
 
 # ---Performance of prediction models---
@@ -44,3 +44,22 @@ prediction_data['model'] = prediction_data['model'].str.replace('_train_r2', '')
 sns.boxplot(x='model', y='r2', hue='split', data=prediction_data)
 plt.ylim(-0.1, 1.1)
 plt.show()
+
+# ---Relationship between constraint evaluation metrics and datasets---
+
+dataset_agg_data = results.groupby('dataset_name')[EVALUATION_METRICS].mean()
+for evaluation_metric in EVALUATION_METRICS:
+    sns.boxplot(x=evaluation_metric, data=dataset_agg_data)
+    if evaluation_metric.endswith('_r2'):
+        plt.xlim(-0.1, 1.1)
+    plt.show()
+
+for i in range(len(EVALUATION_METRICS) - 1):
+    for j in range(i + 1, len(EVALUATION_METRICS)):
+        dataset_agg_data.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j])
+        if EVALUATION_METRICS[j].endswith('_r2'):
+            plt.ylim(-0.1, 1.1)
+        plt.show()
+
+sns.heatmap(data=dataset_agg_data[EVALUATION_METRICS].corr(method='spearman'), vmin=-1, vmax=1,
+            cmap='RdYlGn', annot=True, square=True)
