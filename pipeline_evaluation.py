83dec783fd1fd66a46313300fc94b439819f1d96
Jakob Bach <jakob.bach@kit.edu>
2020-09-01 16:50:10 +0200

    Normalize number of constraints in evaluation
    So now we only deal with normalized quantities for all evaluation metrics
    (apart from the fact that R^2 has no lower bound, but we simply cut that).

--- pipeline_evaluation.py
+++ pipeline_evaluation.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 
 results = pd.read_csv('data/openml-results/results.csv')
-CONSTRAINT_METRICS = ['frac_objective', 'frac_selected', 'num_constraints', 'frac_solutions']
+CONSTRAINT_METRICS = ['frac_objective', 'frac_selected', 'frac_constraints', 'frac_solutions']
 PREDICTION_METRICS = [x for x in results.columns if x.endswith('_r2')]
 EVALUATION_METRICS = CONSTRAINT_METRICS + ['linear-regression_test_r2', 'xgb-tree_test_r2']
 
@@ -21,6 +21,9 @@ assert len(max_objective_values) == results.groupby(['quality_name', 'split_idx'
 max_objective_values.rename(columns={'objective_value': 'max_objective'}, inplace=True)
 results = results.merge(max_objective_values)
 results['frac_objective'] = results['objective_value'] / results['max_objective']
+# Make number of constraints relative (same procedure used for all constraint generators with
+# variable number of constraints anyway, and normalizing allows to use the same ranges on all axes)
+results['frac_constraints'] = results['num_constraints'] / results['num_constraints'].max()
 
 
 def reshape_prediction_data(results_data: pd.DataFrame) -> pd.DataFrame:
@@ -35,17 +38,15 @@ def reshape_prediction_data(results_data: pd.DataFrame) -> pd.DataFrame:
 for evaluation_metric in EVALUATION_METRICS:
     sns.boxplot(x='constraint_name', y=evaluation_metric, data=results)
     plt.xticks(rotation=45)
-    if evaluation_metric.endswith('_r2'):
-        plt.ylim(-0.1, 1.1)
+    plt.ylim(-0.1, 1.1)
     plt.show()
 
 # ---Relationship between constraint evaluation metrics---
 
 for i in range(len(EVALUATION_METRICS) - 1):
     for j in range(i + 1, len(EVALUATION_METRICS)):
-        results.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j], s=1)
-        if EVALUATION_METRICS[j].endswith('_r2'):
-            plt.ylim(-0.1, 1.1)
+        results.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j], s=1,
+                             xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
         plt.show()
 # All in one: sns.pairplot(results[EVALUATION_METRICS])
 
@@ -55,7 +56,8 @@ sns.heatmap(data=results[EVALUATION_METRICS].corr(method='spearman'), vmin=-1, v
 # ---Performance of prediction models---
 
 # also interesting for results.loc[results['constraint_name'] == 'UNCONSTRAINED']
-sns.boxplot(x='model', y='r2', hue='split', data=reshape_prediction_data(results))
+prediction_data = reshape_prediction_data(results)
+sns.boxplot(x='model', y='r2', hue='split', data=prediction_data)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
@@ -64,15 +66,13 @@ plt.show()
 dataset_agg_data = results.groupby('dataset_name')[EVALUATION_METRICS].mean()
 for evaluation_metric in EVALUATION_METRICS:
     sns.boxplot(x=evaluation_metric, data=dataset_agg_data)
-    if evaluation_metric.endswith('_r2'):
-        plt.xlim(-0.1, 1.1)
+    plt.xlim(-0.1, 1.1)
     plt.show()
 
 for i in range(len(EVALUATION_METRICS) - 1):
     for j in range(i + 1, len(EVALUATION_METRICS)):
-        dataset_agg_data.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j])
-        if EVALUATION_METRICS[j].endswith('_r2'):
-            plt.ylim(-0.1, 1.1)
+        dataset_agg_data.plot.scatter(x=EVALUATION_METRICS[i], y=EVALUATION_METRICS[j],
+                                      xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
         plt.show()
 
 sns.heatmap(data=dataset_agg_data[EVALUATION_METRICS].corr(method='spearman'), vmin=-1, vmax=1,
