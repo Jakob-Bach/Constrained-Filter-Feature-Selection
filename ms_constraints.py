96ac1dd4bb58840b43c3ed89f8b05b3b421f7a43
Jakob Bach <jakob.bach@kit.edu>
2020-09-30 18:20:16 +0200

    Add more MS constraint evaluators
    We complement the first two evaluators, add in c8de0b77bc, with some
    more, also making use of the existence of aggregate features. The
    constraints are neither overly systematic nor combined among each other, we
    just tried out how well we can code sensible constraints (pretty well,
    applying pattern matching to feature names).

--- ms_constraints.py
+++ ms_constraints.py
@@ -9,6 +9,10 @@ from typing import Dict
 
 import combi_expressions as expr
 import combi_solving as solv
+import ms_datasets
+
+
+SCHMID_GROUPS_100 = [[1, 2, 5, 6, 7, 8, 11, 12], [3, 4, 9, 10]]  # for (1 0 0) orientation
 
 
 class MSConstraintEvaluator(metaclass=ABCMeta):
@@ -36,17 +40,180 @@ class NoConstraintEvaluator(MSConstraintEvaluator):
         pass
 
 
-class SchmidFactor100Evaluator(MSConstraintEvaluator):
-
-    SLIP_GROUPS = [[1, 2, 5, 6, 7, 8, 11, 12], [3, 4, 9, 10]]  # for (1 0 0) orientation
+# For Schmid factor (1 0 0) grouping, select features from at most one group
+class SelectSchmidGroupEvaluator(MSConstraintEvaluator):
 
     def add_constraints(self) -> None:
         variable_groups = []
-        for slip_group in SchmidFactor100Evaluator.SLIP_GROUPS:
-            variable_group = []
-            for variable in self.problem.get_variables():
-                if re.search('_(' + '|'.join([str(i) for i in slip_group]) + ')$', str(variable.z3)) is not None:
-                    variable_group.append(variable)
+        for slip_group in SCHMID_GROUPS_100:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if re.search('_(' + '|'.join([str(i) for i in slip_group]) + ')$',
+                                           variable.get_name()) is not None]
             variable_groups.append(variable_group)
-        # Select features from at most one of these groups
         self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 1))
+
+
+# For each quantity, for Schmid factor (1 0 0) grouping, select features from at most one group
+class SelectQuantitySchmidGroupEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        base_quantities = [variable.get_name().replace('_1', '') for variable in self.problem.get_variables()
+                           if variable.get_name().endswith('_1')]
+        for quantity in base_quantities:
+            variable_groups = []
+            for slip_group in SCHMID_GROUPS_100:
+                variable_group = [variable for variable in self.problem.get_variables()
+                                  if re.search(quantity + '_(' + '|'.join([str(i) for i in slip_group]) + ')$',
+                                               variable.get_name()) is not None]
+                variable_groups.append(variable_group)
+            self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 1))
+
+
+# For Schmid factor (1 0 0) grouping, select at most one feature from each group
+class SelectSchmidGroupRepresentativeEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        variable_groups = []
+        for slip_group in SCHMID_GROUPS_100:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if re.search('_(' + '|'.join([str(i) for i in slip_group]) + ')$',
+                                           variable.get_name()) is not None]
+            variable_groups.append(variable_group)
+        self.problem.add_constraint(expr.And([expr.AtMost(x, 1) for x in variable_groups]))
+
+
+# For each quantity, for Schmid factor (1 0 0) grouping, select at most one feature from each group
+class SelectQuantitySchmidGroupRepresentativeEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        base_quantities = [variable.get_name().replace('_1', '') for variable in self.problem.get_variables()
+                           if variable.get_name().endswith('_1')]
+        for quantity in base_quantities:
+            variable_groups = []
+            for slip_group in SCHMID_GROUPS_100:
+                variable_group = [variable for variable in self.problem.get_variables()
+                                  if re.search(quantity + '_(' + '|'.join([str(i) for i in slip_group]) + ')$',
+                                               variable.get_name()) is not None]
+                variable_groups.append(variable_group)
+            self.problem.add_constraint(expr.And([expr.AtMost(x, 1) for x in variable_groups]))
+
+
+# For each slip system, select either all quantities from that slip system or none
+class SelectWholeSlipSystemsEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        for slip_system in range(1, 13):
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if variable.get_name().endswith('_' + slip_system)]
+            self.problem.add_constraint(expr.Iff(variable_group))
+
+
+# From reaction features, select features belonging to at most one reaction type
+class SelectReactionTypeEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        variable_groups = []
+        for reaction_type in ms_datasets.REACTION_TYPES:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if reaction_type in variable.get_name()]
+            variable_groups.append(variable_group)
+        self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 1))
+
+
+# For each quantity, select either absolute value or delta value or none
+class SelectValueOrDeltaEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self) -> None:
+        delta_variables = [variable for variable in self.problem.get_variables()
+                           if 'delta_' in variable.get_name()]
+        for delta_variable in delta_variables:
+            variable_name = delta_variable.get_name().replace('delta_', '')
+            for variable in self.problem.get_variables():
+                if variable.get_name() == variable_name:
+                    self.problem.add_constraint(expr.Not(expr.And([variable, delta_variable])))
+                    break
+
+
+# From plastic strain tensor, select at most three directions
+class SelectStrainTensorEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        variable_groups = []
+        directions = [variable.get_name().replace('eps_', '') for variable in self.problem.get_variables()
+                      if re.match('eps_[a-z]{2}$', variable.get_name())]
+        for direction in directions:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if 'eps_' + direction in variable.get_name()]
+            variable_groups.append(variable_group)
+        self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 3))
+
+
+# For dislocation density, select at most one from three feature groups which all describe it
+class SelectDislocationDensityEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        variable_groups = []
+        quantity_patterns = ['rho_(' + '|'.join(ms_datasets.AGGREGATES) + ')',
+                             'mean_free_path', 'free_path_per_voxel']
+        for pattern in quantity_patterns:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if re.search(pattern, variable.get_name()) is not None]
+            variable_groups.append(variable_group)
+        self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 1))
+
+
+# For strain rate computation, select at most one type
+class SelectStrainRateEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        gamma_variables = [variable for variable in self.problem.get_variables()
+                           if 'gamma' in variable.get_name()]
+        gamma_abs_variables = [variable for variable in gamma_variables
+                               if 'gamma_abs' in variable.get_name()]
+        gamma_variables = [variable for variable in gamma_variables
+                           if 'gamma_abs' not in variable.get_name()]
+        self.problem.add_constraint(expr.Not(expr.And([expr.Or(gamma_variables),
+                                                       expr.Or(gamma_abs_variables)])))
+
+
+# Over all quantities, select at most one type of aggregate
+class SelectAggregateEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        variable_groups = []
+        for aggregate in ms_datasets.AGGREGATES:
+            variable_group = [variable for variable in self.problem.get_variables()
+                              if variable.get_name().endswith('_' + aggregate)]
+            variable_groups.append(variable_group)
+        self.problem.add_constraint(expr.AtMost([expr.Or(x) for x in variable_groups], 1))
+
+
+# For each quantity, select at most one type of aggregate
+class SelectQuantityAggregateEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        base_quantities = [variable.get_name().replace('_1', '') for variable in self.problem.get_variables()
+                           if variable.get_name().endswith('_1')]
+        for quantity in base_quantities:
+            aggregate_variables = [variable for variable in self.problem.get_variables()
+                                   if re.search(quantity + '_(' + '|'.join(ms_datasets.AGGREGATES) + ')$',
+                                                variable.get_name()) is not None]
+            if len(aggregate_variables) > 0:
+                self.problem.add_constraint(expr.AtMost(aggregate_variables, 1))
+
+
+# For each quantity, select either aggregates or orignal values or none
+class SelectAggregateOrOriginalEvaluator(MSConstraintEvaluator):
+
+    def add_constraints(self):
+        base_quantities = [variable.get_name().replace('_1', '') for variable in self.problem.get_variables()
+                           if variable.get_name().endswith('_1')]
+        for quantity in base_quantities:
+            original_variables = [variable for variable in self.problem.get_variables()
+                                  if re.search(quantity + '_[0-9]+$', variable.get_name()) is not None]
+            aggregate_variables = [variable for variable in self.problem.get_variables()
+                                   if re.search(quantity + '_(' + '|'.join(ms_datasets.AGGREGATES) + ')$',
+                                                variable.get_name()) is not None]
+            if len(original_variables) > 0 and len(aggregate_variables) > 0:
+                self.problem.add_constraint(expr.Not(expr.And([expr.Or(original_variables),
+                                                               expr.Or(aggregate_variables)])))
