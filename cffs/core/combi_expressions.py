22dc49debcc9c69779d6bc7511ebf39262fbe5a7
Jakob Bach <jakob.bach@kit.edu>
2020-10-15 16:24:48 +0200

    Fix access to z3_expr in combi_solving
    When renaming the attribute "z3" of combi_solving.BooleanExpression to
    "z3_expr" in 9776aaca12, we adapted setter access, but not getter access.
    We fix this now and additionally make sure that getter access goes through
    the corresponding method instead of accessing the attribute directly
    (though the getter does not provide additional functionality at the
    moment).

--- cffs/core/combi_expressions.py
+++ cffs/core/combi_expressions.py
@@ -37,75 +37,75 @@ class And(expr.And, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression]):
         super().__init__(bool_expressions)
-        self.z3_expr = z3.And([x.z3 for x in bool_expressions])
+        self.z3_expr = z3.And([x.get_z3() for x in bool_expressions])
 
 
 class AtLeast(expr.Ge, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression], value: float):
         super().__init__(expr.Sum(bool_expressions), expr.NumericValue(value))
-        self.z3_expr = z3.AtLeast(*[e.z3 for e in bool_expressions], value)
+        self.z3_expr = z3.AtLeast(*[e.get_z3() for e in bool_expressions], value)
 
 
 class AtMost(expr.Le, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression], value: float):
         super().__init__(expr.Sum(bool_expressions), expr.NumericValue(value))
-        self.z3_expr = z3.AtMost(*[e.z3 for e in bool_expressions], value)
+        self.z3_expr = z3.AtMost(*[e.get_z3() for e in bool_expressions], value)
 
 
 class Iff(expr.Iff, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression]):
         super().__init__(bool_expressions)
-        self.z3_expr = z3.Or(z3.And([x.z3 for x in bool_expressions]),
-                             z3.Not(z3.Or([x.z3 for x in bool_expressions])))
+        self.z3_expr = z3.Or(z3.And([x.get_z3() for x in bool_expressions]),
+                             z3.Not(z3.Or([x.get_z3() for x in bool_expressions])))
 
 
 class Implies(expr.Implies, BooleanExpression):
 
     def __init__(self, bool_expression1: expr.BooleanExpression, bool_expression2: expr.BooleanExpression):
         super().__init__(bool_expression1, bool_expression2)
-        self.z3_expr = z3.Implies(bool_expression1.z3, bool_expression2.z3)
+        self.z3_expr = z3.Implies(bool_expression1.get_z3(), bool_expression2.get_z3())
 
 
 class Not(expr.Not, BooleanExpression):
 
     def __init__(self, bool_expression: expr.BooleanExpression):
         super().__init__(bool_expression)
-        self.z3_expr = z3.Not(bool_expression.z3)
+        self.z3_expr = z3.Not(bool_expression.get_z3())
 
 
 class Or(expr.Or, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression]):
         super().__init__(bool_expressions)
-        self.z3_expr = z3.Or([x.z3 for x in bool_expressions])
+        self.z3_expr = z3.Or([x.get_z3() for x in bool_expressions])
 
 
 class WeightedSumEq(expr.Eq, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression], weights: Sequence[float], value: float):
         super().__init__(expr.WeightedSum(bool_expressions, weights), expr.NumericValue(value))
-        self.z3_expr = z3.PbEq([(e.z3, w) for (e, w) in zip(bool_expressions, weights)], value)
+        self.z3_expr = z3.PbEq([(e.get_z3(), w) for (e, w) in zip(bool_expressions, weights)], value)
 
 
 class WeightedSumGe(expr.Ge, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression], weights: Sequence[float], value: float):
         super().__init__(expr.WeightedSum(bool_expressions, weights), expr.NumericValue(value))
-        self.z3_expr = z3.PbGe([(e.z3, w) for (e, w) in zip(bool_expressions, weights)], value)
+        self.z3_expr = z3.PbGe([(e.get_z3(), w) for (e, w) in zip(bool_expressions, weights)], value)
 
 
 class WeightedSumLe(expr.Le, BooleanExpression):
 
     def __init__(self, bool_expressions: Sequence[expr.BooleanExpression], weights: Sequence[float], value: float):
         super().__init__(expr.WeightedSum(bool_expressions, weights), expr.NumericValue(value))
-        self.z3_expr = z3.PbLe([(e.z3, w) for (e, w) in zip(bool_expressions, weights)], value)
+        self.z3_expr = z3.PbLe([(e.get_z3(), w) for (e, w) in zip(bool_expressions, weights)], value)
 
 
 class Xor(expr.Xor, BooleanExpression):
 
     def __init__(self, bool_expression1: expr.BooleanExpression, bool_expression2: expr.BooleanExpression):
         super().__init__(bool_expression1, bool_expression2)
-        self.z3_expr = z3.Xor(bool_expression1.z3, bool_expression2.z3)
+        self.z3_expr = z3.Xor(bool_expression1.get_z3(), bool_expression2.get_z3())
