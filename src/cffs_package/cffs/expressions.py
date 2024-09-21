c42f79b95c1b35326cf0bcd970726a6a8ca5fca5
Jakob Bach <jakob.bach@kit.edu>
2024-09-21 16:36:16 +0200

    Use proper docstrings for SMT-expression classes
    Increase user-friendliness as preparation step to release the Python
    package.

--- src/cffs_package/cffs/expressions.py
+++ src/cffs_package/cffs/expressions.py
@@ -1,7 +1,13 @@
 """SMT expressions
 
-Classes representing logical and arithmetic expressions that allow to formulate constraints.
-We use an object-oriented design, so constraint evaluation can be dispatched dynamically.
+Classes representing logical and arithmetic expressions that enable users to formulate constraints
+for feature selection. The object-oriented design allows expressions to be nested and their
+evaluation to be dispatched dynamically.
+
+Literature
+----------
+- Bach et al. (2022): "An Empirical Evaluation of Constrained Feature Selection"
+- Barrett & Tinelli (2018): "Satisfiability Modulo Theories"
 """
 
 from __future__ import annotations  # to use a class as a type hint within its own definition
@@ -11,24 +17,64 @@ from typing import Sequence
 
 
 class Expression(metaclass=ABCMeta):
+    """SMT expression
 
-    # Should allow to iterate over the expression tree. Some expressions might not have any
-    # children, so this default implementation is sufficient. In contrast, if an expression
-    # nests one or multiple child expressions, this method should be overridden in sub-classes.
+    Should evaluate to an arithmetic or boolean value (subclasses should implement corresponding
+    evaluation methods). Can nest multiple child expressions.
+    """
+
     def get_children(self) -> Sequence[Expression]:
+        """Get child expressions
+
+        This function allows to iterate over the expression tree down to the leaves (expressions
+        without children).
+
+        Returns
+        -------
+        Sequence[Expression]
+            The child expressions. By default, there are none (e.g., for a constant value).
+            For arithmetic or logical operators, the child expressions are its operands, and the
+            method should be overridden accordingly in subclasses. Also, you should define an
+            initializer to set the child expressions.
+        """
+
         return []
 
 
-# Expression that evaluates to true or false.
 class BooleanExpression(Expression, metaclass=ABCMeta):
+    """Boolean expression
+
+    Evaluates to true or false.
+    """
 
     @abstractmethod
     def is_true(self) -> bool:
+        """Evaluate boolean expression
+
+        Should determine the value of the boolean expression, typically by evaluating its child
+        expressions (operands) first and then applying the operator represented by this expression.
+        See the corresponding subclass description for details.
+
+        Raises
+        ------
+        NotImplementedError
+            Always raised since abstract method (implementation depends on logical operator).
+
+        Returns
+        -------
+        bool
+            Boolean value of the expression.
+        """
+
         raise NotImplementedError('Abstract method.')
 
 
-# An elementary boolean expression, where you can set the value directly.
 class BooleanValue(BooleanExpression):
+    """Boolean value
+
+    Elementary logical expression that evaluates to a user-provided boolean value. Does not have
+    child expressions.
+    """
 
     def __init__(self, value: bool):
         self.value = value
@@ -40,14 +86,26 @@ class BooleanValue(BooleanExpression):
         return self.is_true()
 
 
-# An elementary boolean expression with a name.
 class Variable(BooleanValue):
+    """Boolean variable
+
+    A boolean value with an additional name attribute. Represents a decision variable in the
+    optimization problem of constrained feature selection.
+    """
 
     def __init__(self, name: str):
         super().__init__(value=False)
         self.name = name
 
     def get_name(self) -> str:
+        """Get name
+
+        Returns
+        -------
+        str
+            The name of the variable, as set during initialization.
+        """
+
         return self.name
 
     def __str__(self) -> str:
@@ -55,6 +113,11 @@ class Variable(BooleanValue):
 
 
 class And(BooleanExpression):
+    """Logical AND
+
+    Evaluates to true if and only if all its child expressions evaluate to true. Can have an
+    arbitrary number of child expressions.
+    """
 
     def __init__(self, bool_expressions: Sequence[BooleanExpression]):
         self.bool_expressions = bool_expressions
@@ -69,8 +132,13 @@ class And(BooleanExpression):
         return self.bool_expressions
 
 
-# Logical equivalence: x_1 <-> ... <-> x_n.
 class Iff(BooleanExpression):
+    """Logical IFF (equivalence)
+
+    Evaluates to true if and only if all its child expressions evaluate to the same value, i.e.,
+    all evaluate to true or all evaluate to false. Can have an arbitrary number of child
+    expressions.
+    """
 
     def __init__(self, bool_expressions: Sequence[BooleanExpression]):
         self.bool_expressions = bool_expressions
@@ -87,6 +155,11 @@ class Iff(BooleanExpression):
 
 
 class Implies(BooleanExpression):
+    """Logical IF (implication)
+
+    Evaluates to true if and only if its first child expression evaluates to false or its second
+    child expression evaluates to true. Has exactly two child expressions.
+    """
 
     def __init__(self, bool_expression1: BooleanExpression, bool_expression2: BooleanExpression):
         self.bool_expression1 = bool_expression1
@@ -100,6 +173,11 @@ class Implies(BooleanExpression):
 
 
 class Not(BooleanExpression):
+    """Logical NOT (negation)
+
+    Evaluates to true if and only if its child expression evaluates to false. Has exactly one child
+    expression.
+    """
 
     def __init__(self, bool_expression: BooleanExpression):
         self.bool_expression = bool_expression
@@ -112,6 +190,11 @@ class Not(BooleanExpression):
 
 
 class Or(BooleanExpression):
+    """Logical (inclusive) OR
+
+    Evaluates to true if and only if at least one of its child expression evaluates to true. Can
+    have an arbitrary number of child expressions.
+    """
 
     def __init__(self, bool_expressions: Sequence[BooleanExpression]):
         self.bool_expressions = bool_expressions
@@ -126,8 +209,12 @@ class Or(BooleanExpression):
         return self.bool_expressions
 
 
-# Exclusive OR.
 class Xor(BooleanExpression):
+    """Logical exclusive OR
+
+    Evaluates to true if and only if its two child expressions evaluate to different boolean
+    values. Has exactly two child expressions.
+    """
 
     def __init__(self, bool_expression1: BooleanExpression, bool_expression2: BooleanExpression):
         self.bool_expression1 = bool_expression1
@@ -140,16 +227,40 @@ class Xor(BooleanExpression):
         return [self.bool_expression1, self.bool_expression2]
 
 
-# Expression that evaluates to a number.
 class ArithmeticExpression(Expression, metaclass=ABCMeta):
+    """Arithmetic expression
+
+    Evaluates to a numeric value.
+    """
 
     @abstractmethod
     def get_value(self) -> float:
+        """Evaluate arithmetic expression
+
+        Should determine the value of the arithmetic expression, typically by evaluating its child
+        expressions (operands) first and then applying the operator represented by this expression.
+        See the corresponding subclass description for details.
+
+        Raises
+        ------
+        NotImplementedError
+            Always raised since abstract method (implementation depends on arithmetic operator).
+
+        Returns
+        -------
+        float
+            Numeric value of the expression.
+        """
+
         raise NotImplementedError('Abstract method.')
 
 
-# An elementary arithmetic expression, where you can set the value directly.
 class NumericValue(ArithmeticExpression):
+    """Numeric value
+
+    Elementary arithmetic expression that evaluates to a user-provided numeric value. Does not
+    have child expressions.
+    """
 
     def __init__(self, value: float):
         self.value = value
@@ -158,10 +269,15 @@ class NumericValue(ArithmeticExpression):
         return self.value
 
 
-# Operator ==
 class Eq(BooleanExpression):
+    """Equality (==) of numeric values
 
-    def __init__(self, arith_expression1: ArithmeticExpression, arith_expression2: ArithmeticExpression):
+    Evaluates to true if and only if its two arithmetic child expressions evaluate to the same
+    numeric value. Has exactly two child expressions.
+    """
+
+    def __init__(self, arith_expression1: ArithmeticExpression,
+                 arith_expression2: ArithmeticExpression):
         self.arith_expression1 = arith_expression1
         self.arith_expression2 = arith_expression2
 
@@ -172,10 +288,15 @@ class Eq(BooleanExpression):
         return [self.arith_expression1, self.arith_expression2]
 
 
-# Operator >=
 class Ge(BooleanExpression):
+    """Greater-or-equal (>=) of numeric values
 
-    def __init__(self, arith_expression1: ArithmeticExpression, arith_expression2: ArithmeticExpression):
+    Evaluates to true if and only if its first arithmetic child expressions evaluate to the same
+    or a greater numeric value than the second. Has exactly two child expressions.
+    """
+
+    def __init__(self, arith_expression1: ArithmeticExpression,
+                 arith_expression2: ArithmeticExpression):
         self.arith_expression1 = arith_expression1
         self.arith_expression2 = arith_expression2
 
@@ -186,10 +307,15 @@ class Ge(BooleanExpression):
         return [self.arith_expression1, self.arith_expression2]
 
 
-# Operator <=
 class Le(BooleanExpression):
+    """Less-or-equal (<=) of numeric values
 
-    def __init__(self, arith_expression1: ArithmeticExpression, arith_expression2: ArithmeticExpression):
+    Evaluates to true if and only if its first arithmetic child expressions evaluate to the same
+    or a smaller numeric value than the second. Has exactly two child expressions.
+    """
+
+    def __init__(self, arith_expression1: ArithmeticExpression,
+                 arith_expression2: ArithmeticExpression):
         self.arith_expression1 = arith_expression1
         self.arith_expression2 = arith_expression2
 
@@ -200,8 +326,14 @@ class Le(BooleanExpression):
         return [self.arith_expression1, self.arith_expression2]
 
 
-# Note that we only sum over boolean values here. This is sufficient for cardinality constraints.
 class Sum(ArithmeticExpression):
+    """Sum (+) of boolean values
+
+    Evaluates to a numeric value that equals the sum of the values to which its child expressions
+    evaluate. Can have an arbitrary number of child expressions. Note that this expression only
+    supports boolean child expressions, since the constraints we analyzed did not require summing
+    actual numeric values.
+    """
 
     def __init__(self, bool_expressions: Sequence[BooleanExpression]):
         self.bool_expressions = bool_expressions
@@ -217,8 +349,14 @@ class Sum(ArithmeticExpression):
         return self.bool_expressions
 
 
-# Sum over boolean values which are weighted with (constant) floats.
 class WeightedSum(ArithmeticExpression):
+    """Weighted sum of numeric values
+
+    Evaluates to a numeric value that equals the sum of the values to which its child expressions
+    evaluate multiplied by user-provided weights (one for each child expression). Can have an
+    arbitrary number of child expressions. Note that this expression only supports boolean child
+    expressions, since the constraints we analyzed did not require summing actual numeric values.
+    """
 
     def __init__(self, bool_expressions: Sequence[BooleanExpression], weights: Sequence[float]):
         assert len(bool_expressions) == len(weights)
@@ -236,9 +374,24 @@ class WeightedSum(ArithmeticExpression):
         return self.bool_expressions
 
 
-# Search an expression tree (recursively) and find all variables. Return a flat list.
-# The same variable might occur multiple times.
 def get_involved_variables(expression: Expression) -> Sequence[Variable]:
+    """Get variables involved in expression
+
+    Traverses the tree formed by the expression (i.e., all child expressions recursively) and
+    extracts all boolean expressions representing decision variables.
+
+    Parameters
+    ----------
+    expression : Expression
+        The expression in which variables should be searched.
+
+    Returns
+    -------
+    Sequence[Variable]
+        The found variables. A variable may appear multiple times if it occurs in multiple
+        subexpressions.
+    """
+
     if isinstance(expression, Variable):
         return [expression]
     if len(expression.get_children()) == 0:
